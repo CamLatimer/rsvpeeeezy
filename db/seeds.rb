c7d8949f921ea46e70ee98ca5b73d9d8115eb6ef
Cam <camlatimer@gmail.com>
2016-03-24 00:09:47 -0400

    prepped seed.rb and commented out index action for events

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,11 +14,11 @@ guests.destroy_all
 
 11.times do |event_num|
 
-  my_event = Event.create(name: "fake Event #{event_num}", address: "421 19th st NE", city: "Washington, DC", day: 2, month: 27, year: 2017, start_time: "11:00am", end_time: '3:30pm', description: "best time ever!!!!!!!!!!!!!", img_url: 'http://happybirthdaycake.org/wp-content/uploads/2015/06/birthday-cake-images-5.jpg')
+  my_event = Event.create(name: "fake Event #{event_num}", address: "1800 19th st NE", city: "Washington, DC", day: 2, month: 27, year: 2017, start_time: "11:00pm", end_time: '4:00am', description: "best time ever!!!!!!!!", img_url: 'http://allimagespictures.org/wp-content/uploads/2015/02/21684.gif')
 
-  4.times do |guest_num|
+  30.times do |guest_num|
 
-    my_event.guests.create(name: "fake guest#{guest_num}", email: "fake_guest@fakefakefakefakestuff.com", message: "i'll be there!!!")
+    my_event.guests.create(name: "fake guest#{guest_num}", email: "fake_guest#{guest_num}@fakefakefakestuff.com", message: "i'll be there!!!")
 
   end
 
